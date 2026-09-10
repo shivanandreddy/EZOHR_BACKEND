79e356d5009dc379b06913d47330212a4b9543db
Shivanand <shivanandreddy1@gmail.com>
2026-09-10 11:53:57 +0530

    added test route

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
