9ceae29810c9816faeb2ca7a4cd850042db9667a
Shivanand <shivanandreddy1@gmail.com>
2026-09-10 17:12:19 +0530

    updated user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -107,7 +107,7 @@ export const login = async (req, res) => {
     }
 
     // Check password
-    const passwordMatch = await bcrypt.compare(password, user.rows[0].password_hash);
+    const passwordMatch = await bcrypt.compare(password, user.rows[0].password);
     if (!passwordMatch) {
       return res.status(401).json({ error: 'Invalid email or password.' });
     }
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Health Check Endpoint
 
